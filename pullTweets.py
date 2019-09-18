b860a7a671dc14d7183e1a0172029a7c8b5cf95a
Jeremiah Sullivan <sully42@gmail.com>
2019-09-18 17:14:26 -0400

    updating output directory structure. Unable to pull more than 200 tweets

--- pullTweets.py
+++ pullTweets.py
@@ -2,4 +2,5 @@
 from tweetImport import tweetImport
 
 a = tweetImport()
-a.analyzeUsername('brabbott42')
+# a.analyzeUsername('brabbott42', range(0, 1000, 200))
+a.analyzeUsername('realdonaldtrump', range(0, 1000, 200))
--- tweetImport.py
+++ tweetImport.py
@@ -8,7 +8,10 @@ from datetime import datetime
 import sys
 import subprocess
 import subprocess
-from   os.path import isfile
+from   os.path import isfile, sep, isdir
+from   os      import makedirs, remove
+# Debug
+from time import sleep
 
 class tweetImport():
     def __init__(self): 
@@ -17,12 +20,42 @@ class tweetImport():
         self.access_token        = '/Users/JP-Macbook/Documents/ec601/miniproject01/tweet_disco/twitter_access.token'
         self.access_token_secret = '/Users/JP-Macbook/Documents/ec601/miniproject01/tweet_disco/twitter_access_secret.token'
 
-    def analyzeUsername(self, username):
-        print('This is working')
+    def makeoutputfolder(self):
+        datestr = datetime.now().strftime('%Y_%m_%d')
+        timestr = datetime.now().strftime('%H_%M%S')
+        if not isdir('output'):
+            makedirs('output'); 
+        curFolder = 'output' + sep + datestr
+        if not isdir(curFolder):
+            makedirs(curFolder)
+
+        curFolder = curFolder + sep + self.user
+        if not isdir(curFolder):
+            makedirs(curFolder)
+        else: 
+            # This logic is useful if we are iterating over multiple tweets
+            i = 1
+            temp = curFolder
+            while isdir(temp):
+                temp = curFolder + '_' + str(i)
+                i += 1; 
+            makedirs(temp)
+            curFolder  = temp; 
+        self.curFolder = curFolder; 
+
+
+
+    def analyzeUsername(self, username, since_id):
         self.user = username
-        api    = self.makeTwitterApi();
-        tweets = self.analyzeUser(api); 
-        self.writeTweetData(tweets); 
+        self.makeoutputfolder(); 
+
+        for ids in since_id:
+            print('The id being sent is: ' + str(ids))
+            # Throw a try/catch here in case we give an invalid since-id
+            api    = self.makeTwitterApi();
+            tweets = self.analyzeUser(api, ids); 
+            self.writeTweetData(tweets); 
+            sleep(2)
 
     def getKeyFromTxt(self, fName):
         if isfile(fName):
@@ -37,25 +70,33 @@ class tweetImport():
     def makeTwitterApi(self):
 
         api = twitter.Api(consumer_key        = self.getKeyFromTxt(self.consumer_key),        \
-                          consumer_secret     = self.getKeyFromTxt(self.consumer_secret),     \
-                          access_token_key    = self.getKeyFromTxt(self.access_token),        \
-                          access_token_secret = self.getKeyFromTxt(self.access_token_secret), \
-                          tweet_mode          = 'extended') 
+                          consumer_secret     = self.getKeyFromTxt(self.consumer_secret),      \
+                          access_token_key    = self.getKeyFromTxt(self.access_token),          \
+                          access_token_secret = self.getKeyFromTxt(self.access_token_secret),    \
+                          tweet_mode          = 'extended'                                    ) 
         return(api)
 
 
-    def analyzeUser(self, api):
+    def analyzeUser(self, api, since_id):
         usertoanalyze = self.user
-        tweets = api.GetUserTimeline(screen_name = usertoanalyze, count     = 200,    \
-                                          include_rts = False,         since_id  = '',    \
-                                          max_id      = '',            trim_user = False, \
-                                          exclude_replies = True                          \
+        print('')
+        print('The since_id is: ' + str(since_id))
+        print('')
+        tweets = api.GetUserTimeline(screen_name = usertoanalyze,             count  = 1000,      \
+                                          include_rts     = False,         since_id  = since_id,       \
+                                          max_id          = '', trim_user = False,    \
+                                          exclude_replies = True
                                         )
+        print(tweets)
+        for dates in tweets.Created
         return tweets
     def writeTweetData(self, stringData):
-        timestr = datetime.now().strftime('%Y_%m_%d_%H%M')
-        fName = 'tweetData_' + self.user + '_' + timestr + '.txt'
-        fid = open(fName, 'w')
+
+        
+
+
+        fName = self.curFolder + sep + 'tweetData_' + self.user + '.txt'
+        fid = open(fName, 'w+')
         for s in stringData:
             text = s.full_text
             urlstrip = re.sub(r"http\S+", "", text) # Remove url
